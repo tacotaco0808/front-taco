2b9393b341cc8089db849fe631e5bd11d258f095
tacotaco0808 <takuto1488@gmail.com>
2025-06-18 18:01:14 +0900

    画像リスト

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,15 +1,19 @@
 import styles from "./Home.module.scss";
 import HomeBackground from "../assets/ドット部屋.png";
-import { SimpleGallery } from "../components/SimpleGallery";
-import { ImagesGallery } from "../components/ImagesGallery";
+
+import { ImagesList } from "../components/ImagesList";
 export const Home = () => {
   return (
     <div>
       <div className={styles.mv_wrapper}>
         <img className={styles.mv_background} src={HomeBackground} alt="" />
+        <div className={styles.gallery_wrapper}>
+          {/* <ImagesGallery /> */}
+          <ImagesList />
+        </div>
       </div>
+
       {/* <SimpleGallery publicId="e8f82240-61a0-4ef0-a6a3-92e031d0d01e" /> */}
-      <ImagesGallery />
     </div>
   );
 };
