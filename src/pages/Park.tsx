1f35acf5277616a5d3cacc795f271914d0b802c3
tacotaco0808 <takuto1488@gmail.com>
2025-08-12 10:37:51 +0900

    #34 受信イベントの処理方法変更。

--- src/pages/Park.tsx
+++ src/pages/Park.tsx
@@ -1,13 +1,14 @@
 import { useEffect, useRef, useState } from "react";
 import type { UserData } from "../phaser/types/PhaserTypes";
 import { fetchUserData } from "../func/fetchUserData";
-import { v4 as uuidv4, v4 } from "uuid";
+// import { v4 as uuidv4, v4 } from "uuid";
 import { PhaserGame } from "../components/PhaserGame";
 import { WsEventHandler } from "../func/wsEventHandler";
 export const Park = () => {
   const [userData, setUserData] = useState<UserData | null>();
   const [scene, setScene] = useState<Phaser.Scene | null>();
   const wsEventHandler = useRef<WsEventHandler>(new WsEventHandler()).current;
+  const [eventQueue, setEventQueue] = useState<any[]>([]);
   useEffect(() => {
     const getMe = async () => {
       const me = await fetchUserData();
@@ -20,30 +21,34 @@ export const Park = () => {
   useEffect(() => {
     if (scene) {
       wsEventHandler.setScene(scene);
+      if (eventQueue.length > 0) {
+        console.log("こいつら実行" + eventQueue);
+        eventQueue.forEach((data) => {
+          wsEventHandler.handle(data);
+        });
+        setEventQueue([]); // キューをクリア
+      }
     }
-  }, [scene]);
+  }, [scene, eventQueue]);
+  // useEffect(() => {
+  //   console.log("test");
+  // }, [eventQueue]);
   useEffect(() => {
     if (!userData) return;
-    const userDataMock = uuidv4();
+    console.log("aiuei");
     const ws = new WebSocket(`ws://localhost:8000/ws/` + userData.user_id);
     ws.onopen = () => {
       ws.send("接続完了");
     };
     ws.onmessage = (event) => {
-      if (!setScene) return;
-      const data = JSON.parse(event.data);
-      wsEventHandler.handle(data);
-      console.log(data);
+      try {
+        const data = JSON.parse(event.data);
+        console.log("受信:", data);
+        setEventQueue((prev) => [...prev, data]);
+      } catch (error) {
+        console.log(event.data);
+      }
     };
-    //parkシーンを表示 GameStart.tsでシーンを変更できるようにする。
-    //
-    //例えば"park"を引数に入れたら、boot preloader "park"　の順に画面遷移
-    //シーンのインスタンスを生成してそれを引数に渡せばよい
-    //parkシーンから座標を取得
-    //座標をこのコンポーネントからwsへ送信
-    //座標をサーバから全プレイヤーへ配信
-    //全クライアントの座標を取得
-    //全クライアントの座標を描画
   }, [userData]);
   return (
     <>
