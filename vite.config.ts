d7b8273f58258e0d50db3a8386f6ba9039d57089
tacotaco0808 <takuto1488@gmail.com>
2025-09-27 21:04:25 +0900

    SSL設定のためのファイルパスを修正し、開発環境でのサーバ設定を整理

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,25 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
-// import fs from "fs";
-// https://vite.dev/config/
-export default defineConfig({
+import fs from "fs";
+import path from "path";
+import os from "os";
+
+//commandにserveを設定し、開発環境での起動のみSSL通信に必要な公開・暗号鍵を参照
+export default defineConfig((command) => ({
   plugins: [react()],
-  // server: {
-  //   https: {
-  //     key: fs.readFileSync("/home/ubuntu/ssl/localhost+2-key.pem"),
-  //     cert: fs.readFileSync("/home/ubuntu/ssl/localhost+2.pem"),
-  //   },
-  //   host: "localhost",
-  //   port: 5173,
-  // },
-});
+  server:
+    command.command === "serve"
+      ? {
+          https: {
+            key: fs.readFileSync(
+              path.join(os.homedir(), ".ssl", "localhost+2-key.pem")
+            ),
+            cert: fs.readFileSync(
+              path.join(os.homedir(), ".ssl", "localhost+2.pem")
+            ),
+          },
+          host: "localhost",
+          port: 5173,
+        }
+      : undefined,
+}));
